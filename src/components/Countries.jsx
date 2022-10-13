8417ac2d54064d8767348682a4ff385cbc5883ad
JaRoD41 <laurent.trinco@gmail.com>
2022-10-13 18:07:13 +0200

    mise en place axios et useState

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import axios from 'axios'
+import React, {useEffect, useState} from 'react'
 
 export default function Countries() {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all")
+    .then((res) => setData(res.data))
+    .catch((err) => console.log(err))
+  }, [])
+  
+
   return (
     <div className='countries'>
       <h1>COUNTRIES</h1>
